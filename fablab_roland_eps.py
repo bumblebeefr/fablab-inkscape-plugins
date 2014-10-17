60ec7238018430f5831bb89fa9f4f22f34a8514c
Bumblebee <git@bumblebee.cc>
2014-10-17 22:29:39 +0200

    Remove dummy comments

--- fablab_roland_eps.py
+++ fablab_roland_eps.py
@@ -14,23 +14,9 @@ def print_(*arg):
     f.write("\n")
     f.close()
 
-# class MyEffect(inkex.Effect):
-#   def __init__(self):
-#     inkex.Effect.__init__(self)
-#     self.OptionParser.add_option("--tab",
-#                       action="store", type="string",
-#                       dest="tab")
-# 
-#   def output(self):
-#       pass
-# 
-#   def effect(self):
-#     print_(sys.argv)
-#     shutil.copyfile(sys.argv[2], "/tmp/tempfile.txt")
-#     pass
 
 if __name__ == '__main__':
-    #shutil.copyfile(sys.argv[2], "/tmp/tempfile.txt")
+
     with open(sys.argv[2]) as f:
         endcomments = False
         for line in f:
