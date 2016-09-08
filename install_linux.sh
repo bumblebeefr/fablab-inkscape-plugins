9ec4652e60c9d687861239a86d43e5bee082f4cb
Bumblebee <git@bumblebee.cc>
2016-09-08 14:41:21 +0200

    Create palette directory

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git pull
 mkdir -p ~/.config/inkscape/extensions/
+mkdir -p ~/.config/inkscape/palettes/
 ln -f -s $(pwd)/fablab* ~/.config/inkscape/extensions/
 ln -f -s $(pwd)/palettes/* ~/.config/inkscape/palettes/
